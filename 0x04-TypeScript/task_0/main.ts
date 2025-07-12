6c89a0ec32f34e923c05a5bd5c7cc636b3262918
Collins Kipruto <collinstunge005@gmail.com>
2025-07-12 11:44:42 +0300

    submit

--- 0x04-TypeScript/task_0/main.ts
+++ 0x04-TypeScript/task_0/main.ts
@@ -1,1 +1,23 @@
+interface Student{
+    firstName: string;
+    lastName: string;
+    age: number;
+    location: string;
+}
 
+const student1: Student = {
+    firstName: 'Collins',
+    lastName: 'Tunge',
+    age: 28,
+    location: 'New York',
+
+}
+const student2: Student = {
+    firstName: 'John',
+    lastName: 'Tunge',
+    age: 48,
+    location: 'New Jersey',
+
+}
+
+const studentsList=[student1,student2]
