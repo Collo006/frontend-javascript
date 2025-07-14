cbc59173a0afbed05d7993a2b771d60e151e9704
Collins Kipruto <collinstunge005@gmail.com>
2025-07-14 21:06:25 +0300

    submit

--- 0x04-TypeScript/task_0/js/main.js
+++
@@ -1,5 +0,0 @@
-//create table
-const table = document.createElement("table");
-
-//table header
-const headerRow = document.createElement("table");
--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -20,5 +20,35 @@ const student2: Student = {
     location: 'Kenya'
 }
 
-const studentsList= [student1, student2];
+//students Array
+const studentsList: Student[]= [student1, student2];
 
+//create table
+const table= document.createElement("table");
+
+//table header
+const headerRow = document.createElement("tr");
+["First Name","Location"].forEach((text)=>{
+    const th = document.createElement("th");
+    th.textContent=text;
+    headerRow.appendChild(th);
+});
+table.appendChild(headerRow);
+
+//create a row for each student
+studentsList.forEach((student)=>{
+    const row = document.createElement("tr");
+
+    const firstNameCell = document.createElement("td");
+    firstNameCell.textContent = student.firstName;
+    row.appendChild(firstNameCell);
+
+    const locationCell =  document.createElement("td");
+    locationCell.textContent = student.location;
+    row.appendChild(locationCell);
+
+    table.appendChild(row)
+})
+
+//add the table to the document
+document.body.appendChild(table);
